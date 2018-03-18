fdbd42f722821533b1c4cfbfad7dde4bdd285567
Riley Kilgore <Riley_Kilgore@Outlook.com>
2018-03-17 21:18:17 -0700

    Finished the main part of 09 exercises for IO handling, 09 file now works as the echo command in terminal.

--- 09InputAndOutput.hs
+++ 09InputAndOutput.hs
@@ -3,11 +3,17 @@
  - The program arguments are simply printed to the standard output.
  - If the first argument is -n, this argument is not printed, and no trailing newline is printed
  -}
+import System.Environment
+import Data.Char
+import Data.List
 
-main = undefined
+main = do
+       messages <- getArgs
+       let message = intercalate " " messages
+       putStrLn message
 
 {- Write a lottery number picker
  - This function should take a StdGen instance, and produce a list of six unique numbers between 1 and 49, in numerical order
  -}
-lottery :: StdGen -> [Int]
+--lottery :: StdGen -> [Int]
 lottery gen = undefined
