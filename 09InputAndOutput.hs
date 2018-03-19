ace5498ee8ee93c60f11845a070944f0c889189a
Riley Kilgore <Riley_Kilgore@Outlook.com>
2018-03-19 16:51:42 -0700

    Finished ch 9 exercises for learn you a haskell.

--- 09InputAndOutput.hs
+++ 09InputAndOutput.hs
@@ -4,6 +4,7 @@
  - If the first argument is -n, this argument is not printed, and no trailing newline is printed
  -}
 import System.Environment
+import System.Random
 import Data.Char
 import Data.List
 
@@ -15,5 +16,14 @@ main = do
 {- Write a lottery number picker
  - This function should take a StdGen instance, and produce a list of six unique numbers between 1 and 49, in numerical order
  -}
---lottery :: StdGen -> [Int]
-lottery gen = undefined
+lottery :: StdGen -> [Int]
+lottery gen = lottery' gen []
+
+lottery' :: StdGen -> [Int] -> [Int]
+lottery' gen xs
+         | length xs >= 6 = xs
+         | otherwise =
+         if num `elem` xs
+            then lottery' newGen xs
+            else lottery' newGen (num:xs)
+         where (num, newGen) = randomR (1, 49) gens
